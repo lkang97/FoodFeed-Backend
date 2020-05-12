22648b40f667256298464afa3c9493ec557e67bd
Lisa Kang <lleekkang@gmail.com>
2020-05-12 15:07:19 -0400

    Add likes routes

--- routes/likes.js
+++ routes/likes.js
@@ -3,19 +3,39 @@ const { check } = require("express-validator");
 const { asyncHandler, handleValidationErrors } = require("../utils");
 
 const db = require("../db/models");
-const { User, Post, Comment } = db;
+const { Like } = db;
 
 const router = express.Router();
 
 router.get(
-  "/posts/:postId/comments",
+  "/posts/:postId/likes",
   asyncHandler(async (req, res) => {
     const postId = parseInt(req.params.postId, 10);
-    const comments = await Comment.findAll({
-      where: { postId },
-      order: [["createdAt"]],
-    });
+    const likes = await Like.findAll({ where: { postId } });
+    res.json({ likes });
   })
 );
 
+router.post(
+  "/posts/:postId/likes",
+  asyncHandler(async (req, res) => {
+    const postId = parseInt(req.params.postId, 10);
+    const { userId } = req.body;
+    const like = await Like.create({ userId, postId });
+    res.status(201).end();
+  })
+);
+
+router.delete(
+  "/likes/:id",
+  asyncHandler(async (req, res) => {
+    const likeId = parseInt(req.params.id, 10);
+    const { userId } = req.body;
+    const like = await Like.findByPk(likeId);
+    if (like && Number(userId) === like.userId) {
+      await like.destroy();
+      res.status(204).end();
+    }
+  })
+);
 module.exports = router;
