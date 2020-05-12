fa1c3f6676504c1c825423a3bc719ac60bc80ee5
Lisa Kang <lleekkang@gmail.com>
2020-05-12 13:27:37 -0400

    Add all posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,63 @@ const express = require("express");
 
 const db = require("../db/models");
 const { User, Post } = db;
+const { getUserToken, requireAuth } = require("../auth");
 const { asyncHandler, handleValidationErrors } = require("../utils");
 
 const router = express.Router();
 
+router.get(
+  "/users/:userId/posts",
+  asyncHandler(async (req, res) => {
+    const userId = parseInt(req.params.userId, 10);
+    const posts = await Post.findAll({ where: { userId } });
+    if (posts) {
+      res.json({ posts });
+    }
+  })
+);
+
+router.get(
+  "/posts",
+  asyncHandler(async (req, res) => {
+    const posts = await Post.findAll();
+    if (posts) {
+      res.json({ posts });
+    }
+  })
+);
+
+router.get(
+  "/posts/:id",
+  asyncHandler(async (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+    const post = await Post.findByPk(postId);
+    if (post) {
+      res.json({ post });
+    }
+  })
+);
+
+router.post(
+  "/posts",
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const { userId, imageUrl, caption } = req.body;
+    const post = await Post.create({ userId, imageUrl, caption });
+    res.status(201).json({ post });
+  })
+);
+
+router.delete(
+  "/posts/:id",
+  asyncHandler(async (req, res) => {
+    const { userId } = req.body;
+    const postId = parseInt(req.params.id, 10);
+    const post = await Post.findByPk(postId);
+    if (post && Number(userId) === post.userId) {
+      await post.destroy();
+      res.status(204).end();
+    }
+  })
+);
 module.exports = router;
