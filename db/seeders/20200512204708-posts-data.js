031331f1f34314f0dde1c3f0d82b950cf59a6e9f
Lisa Kang <lleekkang@gmail.com>
2020-05-18 10:09:45 -0400

    Change seeder files

--- db/seeders/20200512204708-posts-data.js
+++ db/seeders/20200512204708-posts-data.js
@@ -4,11 +4,20 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Posts", [
       {
+        userId: 2,
+        imageUrl:
+          "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/exps8127_HSC143552C08_01_2b.jpg",
+        // imageUrl: "https://foodfeed.s3.us-east-2.amazonaws.com/1589318358364",
+        caption: "It's BEAN good",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
         userId: 1,
         imageUrl:
-          "https://embed.widencdn.net/img/mccormick/xylt4a1x9r/1365x1365px/Supreme%20Pasta%20Salad_2019-04-10_8701_TSUCALAS.jpg?crop=true&anchor=255,0&q=80&color=ffffffff&u=o2hyef",
+          "https://www.simplyrecipes.com/wp-content/uploads/2018/10/HT-Make-an-Omelet-LEAD-VERTICAL.jpg",
         // imageUrl: "https://foodfeed.s3.us-east-2.amazonaws.com/1589318358364",
-        caption: "PASTA-bly the best dish",
+        caption: "What an EGG-cellent omelette",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -32,32 +41,22 @@ module.exports = {
       {
         userId: 2,
         imageUrl:
-          "https://embed.widencdn.net/img/mccormick/xylt4a1x9r/1365x1365px/Supreme%20Pasta%20Salad_2019-04-10_8701_TSUCALAS.jpg?crop=true&anchor=255,0&q=80&color=ffffffff&u=o2hyef",
-        // imageUrl: "https://foodfeed.s3.us-east-2.amazonaws.com/1589318358364",
-        caption: "PASTA-bly the best dish",
+          "https://www.willcookforsmiles.com/wp-content/uploads/2019/01/mac-and-cheese-4.jpg", // imageUrl: "https://foodfeed.s3.us-east-2.amazonaws.com/1589318358364",
+        caption: "CHEESE beautiful",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         userId: 2,
         imageUrl:
-          "https://embed.widencdn.net/img/mccormick/xylt4a1x9r/1365x1365px/Supreme%20Pasta%20Salad_2019-04-10_8701_TSUCALAS.jpg?crop=true&anchor=255,0&q=80&color=ffffffff&u=o2hyef",
+          "https://ifoodreal.com/wp-content/uploads/2017/06/lettuce-tomato-cucumber-salad-recipe-15.jpg",
         // imageUrl: "https://foodfeed.s3.us-east-2.amazonaws.com/1589318358364",
-        caption: "PASTA-bly the best dish",
+        caption: "LETTUCE enjoy this salad",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        userId: 2,
-        imageUrl:
-          "https://embed.widencdn.net/img/mccormick/xylt4a1x9r/1365x1365px/Supreme%20Pasta%20Salad_2019-04-10_8701_TSUCALAS.jpg?crop=true&anchor=255,0&q=80&color=ffffffff&u=o2hyef",
-        // imageUrl: "https://foodfeed.s3.us-east-2.amazonaws.com/1589318358364",
-        caption: "PASTA-bly the best dish",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        userId: 2,
+        userId: 1,
         imageUrl:
           "https://embed.widencdn.net/img/mccormick/xylt4a1x9r/1365x1365px/Supreme%20Pasta%20Salad_2019-04-10_8701_TSUCALAS.jpg?crop=true&anchor=255,0&q=80&color=ffffffff&u=o2hyef",
         // imageUrl: "https://foodfeed.s3.us-east-2.amazonaws.com/1589318358364",
